8d84169768e82dbc2078c20575d17b3dfbccf31b
isinnur <isinngnyy@gmail.com>
2023-11-30 11:20:09 +0300

    feat: setting up firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+//Firebase'den veritabanı işlemleri için Firestore'u kullanmanıza olanak tanır.
+import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,4 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();

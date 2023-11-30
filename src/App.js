825e940da44956fcb26edf0b30dc3d5775e6bf78
isinnur <isinngnyy@gmail.com>
2023-11-30 12:10:42 +0300

    feat: setting up navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <nav>
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/createpost">
+          Crate Post
+        </Link>
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

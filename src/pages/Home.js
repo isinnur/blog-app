3c16b271732b9c9eb0f12efb77f663c5bf5175b6
isinnur <isinngnyy@gmail.com>
2023-12-02 23:28:20 +0300

    feat: display the data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 function Home() {
-  return <div className="homePage"></div>;
+  const [postLists, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "posts");
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  });
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title} </h1>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3> @{post.author.name} </h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;

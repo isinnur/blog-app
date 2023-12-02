1467028ce9fe991f1be10d9c10f9ab2cfdf2ee42
isinnur <isinngnyy@gmail.com>
2023-12-02 22:38:24 +0300

    feat: create post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
 function CreatePost() {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
+  const postCollectionRef = collection(db, "posts");
+  const createPost = async () => {
+    await addDoc();
+  };
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label>Title: </label>
+          <input
+            placeholder="Title..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+        <div className="inputGp">
+          <label>Post: </label>
+          <textarea
+            placeholder="Post..."
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }}
+          />
+        </div>
+        <button>Submit Post</button>
+      </div>
+    </div>
+  );
 }
 
 export default CreatePost;
